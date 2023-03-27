d65a72cb4e36facba5ec1bfb4d7a06774a246135
Caio Teixeira <cepteixeira@gmail.com>
2023-03-27 17:33:53 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('olá mundo'));
+
+console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento');
+console.log('tilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()`');
